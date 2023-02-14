b6d4cb64d929669b631f4c6e5c5f8b76585c6228
JonathasRosa <jonathasrosa85@gmail.com>
2023-02-14 16:41:46 -0300

    Resolução de um novo desafio e adicionando novas palavra ao vocabulario do VsCode.

--- operadores/desafio-02.js
+++ operadores/desafio-02.js
@@ -1,1 +1,16 @@
 //Desafio relacionais
+let a = 1;
+let b = 2;
+
+//Encontrar 5 expressões que o resultado é TRUE.
+console.log(a < b);
+console.log(a != b);
+console.log(a !== b);
+console.log(b > a);
+console.log(b != a);
+//Encontrar 5 expressões que o resultado é FALSE.
+console.log(a > b);
+console.log(a == b);
+console.log(a === b);
+console.log(b < a);
+console.log(b <= a);
