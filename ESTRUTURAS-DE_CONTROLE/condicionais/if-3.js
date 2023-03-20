447fb055cf61bd88225deb2282eae9ac9c38457b
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-03-20 11:25:56 -0300

    Update Curso Lógica de Programação com JavaScript

--- ESTRUTURAS-DE_CONTROLE/condicionais/if-3.js
+++ ESTRUTURAS-DE_CONTROLE/condicionais/if-3.js
@@ -1,7 +1,8 @@
 const nota = 9;
 
-if (nota >= 9)
-    console.log("Quadro de Honra!");
-    console.log("Parabéns");
-    
+if (nota >= 9) {
+    console.log("Quadro de Honra!")
+    console.log("Parabéns")
+}
+       
 console.log('Fim')
