4626929b1cddc56ecf65163b14b6330bf77dc7e2
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-03-24 11:06:19 -0300

    Update Curso Lógica de Programação

---
+++ ARRAYS/explicacao-array.js
@@ -0,0 +1,20 @@
+ /*
+ Array é uma estrutura de dados que armazena várias informações a partir de uma variável.
+ é uma estrutura indexada, com as posições dos elementos iniciando por zero.
+
+               0    1    2
+               Y    Y    Y
+ Ex: notas = [7.2, 8.5, 9.4,]
+ EX: usuarios = [
+    {
+ nome:João
+ nota: 7.2
+ }, {
+ nome: Maria
+ nota: 8.5
+ }, {
+ nome: Claudio
+ nota: 9.4
+ }
+]
+ */
